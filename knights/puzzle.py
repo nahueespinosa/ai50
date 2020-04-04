d54d7df1683a4ec07fb38fe4ce3953aaf5706232
Nahuel Espinosa <nahue.espinosa@gmail.com>
2020-04-04 14:51:33 -0300

    Added knowledge bases 0, 1 and 2

--- knights/puzzle.py
+++ knights/puzzle.py
@@ -12,21 +12,44 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    # A is a knight if and only if A is not a knave
+    Biconditional(AKnight, Not(AKnave)),
+    # If A is a knight then "I am both a knight and a knave" is true
+    Implication(AKnight, And(AKnight, AKnave)),
+    # If A is a knave then "I am both a knight and a knave" is false
+    Implication(AKnave, Not(And(AKnight, AKnave)))
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    # A is a knight if and only if A is not a knave
+    Biconditional(AKnight, Not(AKnave)),
+    # B is a knight if and only if B is not a knave
+    Biconditional(BKnight, Not(BKnave)),
+    # If A is a knight then "We are both knaves" is true
+    Implication(AKnight, And(AKnave, BKnave)),
+    # If A is a knave then "We are both knaves" is false
+    Implication(AKnave, Not(And(AKnave, BKnave)))
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    # A is a knight if and only if A is not a knave
+    Biconditional(AKnight, Not(AKnave)),
+    # B is a knight if and only if B is not a knave
+    Biconditional(BKnight, Not(BKnave)),
+    # If A is a knight then "We are the same kind" is true
+    Implication(AKnight, Or(And(AKnave, BKnave), And(AKnight, BKnight))),
+    # If A is a knave then "We are the same kind" is false
+    Implication(AKnave, Not(Or(And(AKnave, BKnave), And(AKnight, BKnight)))),
+    # If B is a knight then "We are of different kinds" is true
+    Implication(BKnight, Or(And(AKnave, BKnight), And(AKnight, BKnave))),
+    # If B is a knave then "We are of different kinds" is false
+    Implication(BKnave, Not(Or(And(AKnave, BKnight), And(AKnight, BKnave)))),
 )
 
 # Puzzle 3
