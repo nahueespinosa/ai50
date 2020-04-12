151dbdc650b70fb806bb1ae3051bb1d5e308ce5c
Nahuel Espinosa <nahue.espinosa@gmail.com>
2020-04-12 12:21:06 -0300

    First approach to model
    Epochs = 10
    Loss = 3.4986
    Accuracy = 0.0541

--- traffic/traffic.py
+++ traffic/traffic.py
@@ -63,6 +63,7 @@ def load_data(data_dir):
 
     for folder in os.listdir(data_dir):
         folder_path = os.path.join(data_dir, folder)
+        print(f"Loading files from {folder_path}...")
         if os.path.isdir(folder_path):
             for file in os.listdir(folder_path):
                 image = cv2.imread(os.path.join(folder_path, file), cv2.IMREAD_COLOR)
@@ -79,7 +80,36 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    raise NotImplementedError
+    # Create a convolutional neural network
+    model = tf.keras.models.Sequential([
+
+        # Convolutional layer. Learn 32 filters using a 3x3 kernel
+        tf.keras.layers.Conv2D(
+            32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+        ),
+
+        # Max-pooling layer, using 2x2 pool size
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+        # Flatten units
+        tf.keras.layers.Flatten(),
+
+        # Add a hidden layer with dropout
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dropout(0.5),
+
+        # Add an output layer with output units for all categories
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
+    ])
+
+    # Train neural network
+    model.compile(
+        optimizer="adam",
+        loss="categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+
+    return model
 
 
 if __name__ == "__main__":
